a0314c57204fa0322cee60e90a101e1caacaec53
HaarikaR <hramadugu@gmail.com>
2021-08-31 17:24:27 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var employee = {
+    name : "Haary",
+    streetAddress : "41 Kenny cir"
+};
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return Object.assign({} , employee,{
+        [key] : value
+    });
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+   employee[key] = value;
+   return employee;
+}
+function deleteFromEmployeeByKey(employee,key){
+ var newObj = Object.assign({},employee);
+ delete newObj[key];
+ return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
